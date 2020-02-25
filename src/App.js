afce248bf1fc2054f8a360e6edd2dc332af7ca15
eshaamrabaney <eshaam.rabaney@gmail.com>
2020-02-25 13:14:59 +0200

    basic layout and structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
